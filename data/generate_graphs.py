04e5b2d3c2282a4c0b49cf0bc5a9362d8299e0b4
Andrew Luo <ajluo2@illinois.edu>
2021-11-18 00:26:15 -0600

    More MP graphs

--- data/generate_graphs.py
+++ data/generate_graphs.py
@@ -81,20 +81,21 @@ def write_mp_scatter_image(tuples, i, major=None, year=0):
     fig.write_image(filename)
 
 def write_mp_box_image(tuples, i, major=None, year=0):
-    x_flipped = [x for (x, y) in tuples]
-    # Flipped No and yes on accident
-    x = []
-    for flip in x_flipped:
-        if flip == "Yes":
-            x.append("No")
-        else:
-            x.append("Yes")
+    x = [x for (x, y) in tuples]
+    # x_flipped = [x for (x, y) in tuples]
+    # # Flipped No and yes on accident
+    # x = []
+    # for flip in x_flipped:
+    #     if flip == "Yes":
+    #         x.append("No")
+    #     elif flip == "No":
+    #         x.append("Yes")
 
     y = [y for (x, y) in tuples]
     data = pd.DataFrame({"MP " + str(i) + " Grade [%]": y, 
-        "Early Submission": x})
-    fig = px.box(data, x="Early Submission", y="MP " + str(i) + " Grade [%]")
-    filename = "images/mp" + str(i) + "_early_submission"
+        "Number of commits": x})
+    fig = px.box(data, x="Number of commits", y="MP " + str(i) + " Grade [%]")
+    filename = "images/mp" + str(i) + "_number_commits"
     if major is not None:
         filename += "_" + major
     if year != 0:
@@ -104,132 +105,146 @@ def write_mp_box_image(tuples, i, major=None, year=0):
 
 # Generate distributions for exams
 
-for i in range(1,4):
-    tuples = dataStore.getXYTuples("potds", "exam" + str(i))
-    write_exam_histogram_image(tuples, i)
-    write_exam_scatter_image(tuples, i)
+# for i in range(1,4):
+#     tuples = dataStore.getXYTuples("potds", "exam" + str(i))
+#     write_exam_histogram_image(tuples, i)
+#     write_exam_scatter_image(tuples, i)
 
-    for major in majors:
-        tuples = dataStore.getXYTuples("potds", "exam" + str(i), major)
-        write_exam_histogram_image(tuples, i, major)
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "exam" + str(i), None, year)
-        write_exam_histogram_image(tuples, i, None, year)
-    for major in majors:
-        for year in years:
-            tuples = dataStore.getXYTuples("potds", "exam" + str(i), major, year)
-            write_exam_histogram_image(tuples, i, major, year)
+#     for major in majors:
+#         tuples = dataStore.getXYTuples("potds", "exam" + str(i), major)
+#         write_exam_histogram_image(tuples, i, major)
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "exam" + str(i), None, year)
+#         write_exam_histogram_image(tuples, i, None, year)
+#     for major in majors:
+#         for year in years:
+#             tuples = dataStore.getXYTuples("potds", "exam" + str(i), major, year)
+#             write_exam_histogram_image(tuples, i, major, year)
     
 
     
-    for major in majors:
-        tuples = dataStore.getXYTuples("potds", "exam" + str(i), major)
-        write_exam_scatter_image(tuples, i, major)
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "exam" + str(i), None, year)
-        write_exam_scatter_image(tuples, i, None, year)
-    for major in majors:
-        for year in years:
-            tuples = dataStore.getXYTuples("potds", "exam" + str(i), major, year)
-            write_exam_scatter_image(tuples, i, major, year)
+#     for major in majors:
+#         tuples = dataStore.getXYTuples("potds", "exam" + str(i), major)
+#         write_exam_scatter_image(tuples, i, major)
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "exam" + str(i), None, year)
+#         write_exam_scatter_image(tuples, i, None, year)
+#     for major in majors:
+#         for year in years:
+#             tuples = dataStore.getXYTuples("potds", "exam" + str(i), major, year)
+#             write_exam_scatter_image(tuples, i, major, year)
 
 
-tuples = dataStore.getXYTuples("potds", "final_exam")
-write_exam_histogram_image(tuples, 0)
-write_exam_scatter_image(tuples, 0)
+# tuples = dataStore.getXYTuples("potds", "final_exam")
+# write_exam_histogram_image(tuples, 0)
+# write_exam_scatter_image(tuples, 0)
 
-for major in majors:
-    tuples = dataStore.getXYTuples("potds", "final_exam", major)
-    write_exam_histogram_image(tuples, 0, major)
-for year in years:
-    tuples = dataStore.getXYTuples("potds", "final_exam", None, year)
-    write_exam_histogram_image(tuples, 0, None, year)
-for major in majors:
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "final_exam", major, year)
-        write_exam_histogram_image(tuples, 0, major, year)
-
-
-
-for major in majors:
-    tuples = dataStore.getXYTuples("potds", "final_exam", major)
-    write_exam_scatter_image(tuples, 0, major)
-for year in years:
-    tuples = dataStore.getXYTuples("potds", "final_exam", None, year)
-    write_exam_scatter_image(tuples, 0, None, year)
-for major in majors:
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "final_exam", major, year)
-        write_exam_scatter_image(tuples, 0, major, year)
+# for major in majors:
+#     tuples = dataStore.getXYTuples("potds", "final_exam", major)
+#     write_exam_histogram_image(tuples, 0, major)
+# for year in years:
+#     tuples = dataStore.getXYTuples("potds", "final_exam", None, year)
+#     write_exam_histogram_image(tuples, 0, None, year)
+# for major in majors:
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "final_exam", major, year)
+#         write_exam_histogram_image(tuples, 0, major, year)
 
 
 
-Generate distributions for MPs
+# for major in majors:
+#     tuples = dataStore.getXYTuples("potds", "final_exam", major)
+#     write_exam_scatter_image(tuples, 0, major)
+# for year in years:
+#     tuples = dataStore.getXYTuples("potds", "final_exam", None, year)
+#     write_exam_scatter_image(tuples, 0, None, year)
+# for major in majors:
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "final_exam", major, year)
+#         write_exam_scatter_image(tuples, 0, major, year)
+
+
+
+# Generate distributions for MPs
+# for i in range(1,7):
+#     tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i))
+#     write_mp_histogram_image(tuples, i)
+
+#     for major in majors:
+#         tuples = dataStore.getXYTuples("potds", "mp" + str(i), major)
+#         write_mp_histogram_image(tuples, i, major)
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "mp" + str(i), None, year)
+#         write_mp_histogram_image(tuples, i, None, year)
+#     for major in majors:
+#         for year in years:
+#             tuples = dataStore.getXYTuples("potds", "mp" + str(i), major, year)
+#             write_mp_histogram_image(tuples, i, major, year)
+#     write_mp_scatter_image(tuples, i)
+#     for major in majors:
+#         tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), major)
+#         write_mp_scatter_image(tuples, i, major)
+#     for year in years:
+#         tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), None, year)
+#         write_mp_scatter_image(tuples, i, None, year)
+#     for major in majors:
+#         for year in years:
+#             tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), major, year)
+#             write_mp_scatter_image(tuples, i, major, year)
+
+# for i in range(1,7):
+#     tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i))
+#     write_mp_box_image(tuples, i)
+
+#     for major in majors:
+#         tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), major)
+#         write_mp_box_image(tuples, i, major)
+#     for year in years:
+#         tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), None, year)
+#         write_mp_box_image(tuples, i, None, year)
+#     for major in majors:
+#         for year in years:
+#             tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), major, year)
+#             write_mp_box_image(tuples, i, major, year)
 for i in range(1,7):
-    tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i))
-    write_mp_histogram_image(tuples, i)
-
-    for major in majors:
-        tuples = dataStore.getXYTuples("potds", "mp" + str(i), major)
-        write_mp_histogram_image(tuples, i, major)
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "mp" + str(i), None, year)
-        write_mp_histogram_image(tuples, i, None, year)
-    for major in majors:
-        for year in years:
-            tuples = dataStore.getXYTuples("potds", "mp" + str(i), major, year)
-            write_mp_histogram_image(tuples, i, major, year)
-    write_mp_scatter_image(tuples, i)
-    for major in majors:
-        tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), major)
-        write_mp_scatter_image(tuples, i, major)
-    for year in years:
-        tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), None, year)
-        write_mp_scatter_image(tuples, i, None, year)
-    for major in majors:
-        for year in years:
-            tuples = dataStore.getXYTuples("mp"+str(i)+"first_commit_time", "mp" + str(i), major, year)
-            write_mp_scatter_image(tuples, i, major, year)
-
-for i in range(1,7):
-    tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i))
+    tuples = dataStore.getXYTuples("mp"+str(i)+"_commits", "mp" + str(i))
     write_mp_box_image(tuples, i)
 
     for major in majors:
-        tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), major)
+        tuples = dataStore.getXYTuples("mp"+str(i)+"_commits", "mp" + str(i), major)
         write_mp_box_image(tuples, i, major)
     for year in years:
-        tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), None, year)
+        tuples = dataStore.getXYTuples("mp"+str(i)+"_commits", "mp" + str(i), None, year)
         write_mp_box_image(tuples, i, None, year)
     for major in majors:
         for year in years:
-            tuples = dataStore.getXYTuples("mp"+str(i)+"_extra_credit", "mp" + str(i), major, year)
+            tuples = dataStore.getXYTuples("mp"+str(i)+"_commits", "mp" + str(i), major, year)
             write_mp_box_image(tuples, i, major, year)
 
 
-tuples = dataStore.getXYTuples("potds", "final_grade")
-write_exam_histogram_image(tuples, 0)
-write_exam_scatter_image(tuples, 0)
+# tuples = dataStore.getXYTuples("potds", "final_grade")
+# write_exam_histogram_image(tuples, 0)
+# write_exam_scatter_image(tuples, 0)
 
-for major in majors:
-    tuples = dataStore.getXYTuples("potds", "final_grade", major)
-    write_exam_histogram_image(tuples, 0, major)
-for year in years:
-    tuples = dataStore.getXYTuples("potds", "final_grade", None, year)
-    write_exam_histogram_image(tuples, 0, None, year)
-for major in majors:
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "final_grade", major, year)
-        write_exam_histogram_image(tuples, 0, major, year)
+# for major in majors:
+#     tuples = dataStore.getXYTuples("potds", "final_grade", major)
+#     write_exam_histogram_image(tuples, 0, major)
+# for year in years:
+#     tuples = dataStore.getXYTuples("potds", "final_grade", None, year)
+#     write_exam_histogram_image(tuples, 0, None, year)
+# for major in majors:
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "final_grade", major, year)
+#         write_exam_histogram_image(tuples, 0, major, year)
 
 
-for major in majors:
-    tuples = dataStore.getXYTuples("potds", "final_grade", major)
-    write_exam_scatter_image(tuples, 0, major)
-for year in years:
-    tuples = dataStore.getXYTuples("potds", "final_grade", None, year)
-    write_exam_scatter_image(tuples, 0, None, year)
-for major in majors:
-    for year in years:
-        tuples = dataStore.getXYTuples("potds", "final_grade", major, year)
-        write_exam_scatter_image(tuples, 0, major, year)
+# for major in majors:
+#     tuples = dataStore.getXYTuples("potds", "final_grade", major)
+#     write_exam_scatter_image(tuples, 0, major)
+# for year in years:
+#     tuples = dataStore.getXYTuples("potds", "final_grade", None, year)
+#     write_exam_scatter_image(tuples, 0, None, year)
+# for major in majors:
+#     for year in years:
+#         tuples = dataStore.getXYTuples("potds", "final_grade", major, year)
+#         write_exam_scatter_image(tuples, 0, major, year)
