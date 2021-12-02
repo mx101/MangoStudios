51e36f1da0dfafe1755f1c457718abe10a179a25
Heinhold, Megan Joy <mjheinhold@gmail.com>
2021-12-02 00:02:13 -0600

    dropdowns now functional and update global variables, more changes to formatting

--- web/src/views/MPs.vue
+++ web/src/views/MPs.vue
@@ -10,47 +10,46 @@
         </div>
 
         <div class="col">
-            All filters({{this.numFilters}})
+            Filters({{this.numFilters}})
         </div>
 
         <div class="col">
-            <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link mx-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    MP
+                 <select class="form-select" style="width:auto;" v-model="mp" @change="buildFileName('')">
+                    <option disabled value="">MP</option>
+                    <option>mp_intro</option>
+                    <option>mp_stickers</option>
+                    <option>mp_lists</option>
+                    <option>mp_traversals</option>
+                    <option>mp_mosaics</option>
+                    <option>mp_mazes</option>
+                </select>
+                <a v-on:click="clearField('mp')" 
+                    v-if="this.mp.length != 0" 
+                    type="button" 
+                    class="btn btn-primary btn-sm" 
+                    aria-label="Close">Clear
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" >mp_intro</a></li>
-                    <li><a class="dropdown-item" >mp_stickers</a></li>
-                    <li><a class="dropdown-item" >mp_lists</a></li>
-                    <li><a class="dropdown-item" >mp_traversals</a></li>
-                    <li><a class="dropdown-item" >mp_mosaics</a></li>
-                    <li><a class="dropdown-item" >mp_mazes</a></li>
-                </ul>
-            </li>
-            </ul>
         </div>
         
         <div class="col">
-            <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link mx-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Student Behaviors
+                 <select class="form-select" style="width:auto;" v-model="student_behaviors" @change="buildFileName('')">
+                    <option disabled value="">Student Behaviors</option>
+                    <option>Labs completed</option>
+                    <option>POTDS completed</option>
+                    <option>Exam Scores</option>
+                    <option>Final Grade</option>
+                </select>
+                <a v-on:click="clearField('student_behaviors')" 
+                    v-if="this.student_behaviors.length != 0" 
+                    type="button" 
+                    class="btn btn-primary btn-sm" 
+                    aria-label="Close">Clear
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" >Labs</a></li>
-                    <li><a class="dropdown-item" >POTDS</a></li>
-                    <li><a class="dropdown-item" >Exams</a></li>
-                    <li><a class="dropdown-item" >Final Grade</a></li>
-                </ul>
-            </li>
-            </ul>
         </div>
         
         <div class="col">
-            <ul class="navbar-nav ms-auto">
                 <select class="form-select" style="width:auto;" v-model="major" @change="buildFileName('')">
-                    <option disabled value="">Select Major</option>
+                    <option disabled value="">Major</option>
                     <option>CS</option>
                     <option>ECE</option>
                     <option>English</option>
@@ -63,35 +62,22 @@
                     class="btn btn-primary btn-sm" 
                     aria-label="Close">Clear
                 </a>
-                <!-- <li class="nav-item dropdown">
-                    
-                    <a class="nav-link mx-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Major
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" >CS</a></li>
-                        <li><a class="dropdown-item" >CS + X</a></li>
-                        <li><a class="dropdown-item" >Physics</a></li>
-                        <li><a class="dropdown-item" >Electrical Engineering</a></li>
-                    </ul>
-                </li> -->
-            </ul>
         </div>
         
         <div class="col">
-            <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link mx-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Year
+                <select class="form-select" style="width:auto;" v-model="year" @change="buildFileName('')">
+                    <option disabled value="">Year</option>
+                    <option>Freshman</option>
+                    <option>Sophomore</option>
+                    <option>Junior</option>
+                    <option>Senior</option>
+                </select>
+                <a v-on:click="clearField('year')" 
+                    v-if="this.year.length != 0" 
+                    type="button" 
+                    class="btn btn-primary btn-sm" 
+                    aria-label="Close">Clear
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" >Freshman</a></li>
-                    <li><a class="dropdown-item" >Sophomore</a></li>
-                    <li><a class="dropdown-item" >Junior</a></li>
-                    <li><a class="dropdown-item" >Senior</a></li>
-                </ul>
-            </li>
-            </ul>
         </div>
     </div>
 
@@ -168,8 +154,10 @@ export default {
             this.major = ""
         } else if (field == "year") {
             this.year = ""
-        } else if (field == "behavior") {
-            this.behavior = ""
+        } else if (field == "student_behaviors") {
+            this.student_behaviors = ""
+        } else {
+            this.mp = ""
         }
 
         this.buildFileName(this.exam)
